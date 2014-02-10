4177a0f6220a1ffc077364e8e2489c8fc8d21aad
stoyanvi <stoyan.vi@gmail.com>
2014-02-10 15:14:43 +0200

    Added ability to show/add comments for an activity.

--- platform-friends-hybrid/scripts/app/activity.js
+++ platform-friends-hybrid/scripts/app/activity.js
@@ -7,21 +7,110 @@ var app = app || {};
 app.Activity = (function () {
     'use strict'
     
+    var $commentsContainer,
+        $commentsTemplate,
+        listScroller;
+    
+    var commentsModel = (function () {
+        
+        var commentModel = {
+            id: 'Id',
+            fields: {
+                Comment: {
+                    field: 'Comment',
+                    defaultValue: ''
+                },
+                CreatedAt: {
+                    field: 'CreatedAt',
+                    defaultValue: new Date()
+                },
+                ActivityId: {
+                    field: 'ActivityId',
+                    defaultValue: null
+                },
+                UserId: {
+                    field: 'UserId',
+                    defaultValue: null
+                }
+            },
+            CreatedAtFormatted: function () {
+
+                return app.helper.formatDate(this.get('CreatedAt'));
+            },
+            User: function () {
+
+                var userId = this.get('UserId');
+
+                var user = $.grep(app.Users.users(), function (e) {
+                    return e.Id === userId;
+                })[0];
+
+                return user ? user.DisplayName : 'Anonymous';
+            }
+        };
+        
+        var commentsDataSource = new kendo.data.DataSource({
+            type: 'everlive',
+            schema: {
+                model: commentModel
+            },
+            transport: {
+                typeName: 'Comments'
+            },
+            serverFiltering: true,
+            change: function (e) {
+                
+                listScroller.reset();
+
+                if (e.items && e.items.length > 0) {
+                    $commentsContainer.kendoMobileListView({
+                        dataSource: e.items,
+                        template: kendo.template($commentsTemplate.html())
+                    });
+                } else {
+                    $commentsContainer.empty();
+                }
+            },
+            sort: { field: 'CreatedAt', dir: 'desc' }
+        });
+        
+        return {
+            comments: commentsDataSource
+        };
+        
+    }());
+    
     var activityViewModel = (function () {
         
-        var activityUid;
+        var activityUid,
+            activity,
+            $activityPicture;
+        
+        var init = function () {
+            $commentsContainer = $('#comments-listview');
+            $commentsTemplate = $('#commentsTemplate');
+            $activityPicture = $('#picture');
+        };
         
         var show = function (e) {
             
+            $commentsContainer.empty();
+            
+            listScroller = e.view.scroller;
+            listScroller.reset();
+            
             activityUid = e.view.params.uid;
             // Get current activity (based on item uid) from Activities model
-            var activity = app.Activities.activities.getByUid(activityUid);
+            activity = app.Activities.activities.getByUid(activityUid);
+            $activityPicture[0].style.display = activity.Picture ? 'block' : 'none';
+            
+            commentsModel.comments.filter({
+                field: 'ActivityId',
+                operator: 'eq',
+                value: activity.Id
+            });
+            
             kendo.bind(e.view.element, activity, kendo.mobile.ui);
-        };
-        
-        var hide = function () {
-            var picture = document.getElementById('picture');
-            picture.src = '';
         };
         
         var removeActivity = function () {
@@ -34,6 +123,7 @@ app.Activity = (function () {
                 'Delete Activity',
                 function (confirmed) {
                     if (confirmed === true || confirmed === 1) {
+                        
                         activities.remove(activity);
                         activities.one('sync', function () {
                             app.mobileApp.navigate('#:back');
@@ -45,9 +135,13 @@ app.Activity = (function () {
         };
         
         return {
+            init: init,
             show: show,
-            hide: hide,
-            remove: removeActivity
+            remove: removeActivity,
+            comments: commentsModel.comments,
+            activity: function () {
+                return activity;
+            },
         };
         
     }());
---
+++ platform-friends-hybrid/scripts/app/add-comment.js
@@ -0,0 +1,60 @@
+/**
+ * AddActivity view model
+ */
+
+var app = app || {};
+
+app.AddComment = (function () {
+    'use strict'
+
+    var AddCommentViewModel = (function () {
+        
+        var $newComment;
+        var validator;
+        
+        var init = function () {
+            
+            validator = $('#enterComment').kendoValidator().data('kendoValidator');
+            $newComment = $('#newComment');
+        };
+        
+        var show = function () {
+            
+            // Clear field on view show
+            $newComment.val('');
+            validator.hideMessages();
+        };
+        
+        var saveComment = function () {
+            
+            // Validating of the required fields
+            if (validator.validate()) {
+                
+                // Adding new comment to Comments model
+                var comments = app.Activity.comments;
+                var comment = comments.add();
+                
+                comment.Comment = $newComment.val();
+                comment.UserId = app.Users.currentUser.get('data').Id;
+                comment.ActivityId = app.Activity.activity().Id;
+                
+                comments.one('sync', function () {
+                    app.mobileApp.navigate('#:back');
+                });
+                
+                comments.sync();
+            }
+        };
+        
+        return {
+            init: init,
+            show: show,
+            me: app.Users.currentUser,
+            saveComment: saveComment
+        };
+        
+    }());
+    
+    return AddCommentViewModel;
+    
+}());
