1f99463405e3e1780ea954e37fce701741be9acc
martin atanasov <martin.atanasov@telerik.com>
2014-04-16 17:55:20 +0300

    Test updated to support WP8 as well.

--- Tests/specs/friendsSpec.js
+++ Tests/specs/friendsSpec.js
@@ -11,7 +11,9 @@ spec(function() {
 		IOS_URL: 'friends://',
 
 		// NOTE: Update this to the Android package name of the Friends App
-		ANDROID_PACKAGE: 'com.telerik.friends'
+		ANDROID_PACKAGE: 'com.telerik.friends',
+
+		WP8_NAME: 'defaultAppUrl'
 	};
 
 	/**
@@ -45,22 +47,21 @@ spec(function() {
 		'Given Friends is running': {
 			'ios': [
 				ios.launch(configuration.IOS_URL),
+				ios.wait(4000)
 			],
 			'android': [
 				
 				android.launch(configuration.ANDROID_PACKAGE),
 				android.wait(4000)
+			],
+			'wp8': [
+				
+				wp8.launch(configuration.WP8_NAME),
+				wp8.wait(4000)
 			]
 		},
 		'And is logged in': {
-			'ios': [
-				web.setValue(queries.ios.login.usernameField, configuration.USERNAME),
-				web.setValue(queries.ios.login.passwordField, configuration.PASSWORD),
-				web.executeScript('$(targetElement).data("kendoTouch").trigger("tap");', queries.ios.login.loginButton),
-				web.wait(2000),
-			
-			],
-			'android': [
+			'default': [
 				web.setValue(queries.ios.login.usernameField, configuration.USERNAME),
 				web.setValue(queries.ios.login.passwordField, configuration.PASSWORD),
 				web.executeScript('$(targetElement).data("kendoTouch").trigger("tap");', queries.ios.login.loginButton),
@@ -69,54 +70,33 @@ spec(function() {
 			]
 		},
 		'When add is tapped': {
-			'ios': [
-				web.executeScript('$(targetElement).trigger("touchstart").trigger("touchend");', queries.ios.activities.addButton),
-			],
-			'android': [
+			'default': [
 				web.executeScript('$(targetElement).trigger("touchstart").trigger("touchend");', queries.ios.activities.addButton),
 			]
 		},
 
 		'And something on my mind is input': {
-			'ios': [
-				web.setValue(queries.ios.activity.textArea, "Hello World."),
-				web.wait(2000)
-			],
-			'android': [
+			'default': [
 				web.setValue(queries.ios.activity.textArea, "Hello World."),
 				web.wait(2000)
 			]
 		},
 
 		'And post is tapped': {
-			'ios': [
-				web.executeScript('$(targetElement).data("kendoMobileButton").trigger("click");', queries.ios.activity.postButton),
-				web.wait(3000)
-			],
-			'android': [
+			'default': [
 				web.executeScript('$(targetElement).data("kendoMobileButton").trigger("click");', queries.ios.activity.postButton),
 				web.wait(3000)
 			]
 		},
 
 		'And logout is tapped': {
-			'ios': [
-				web.executeScript('$(targetElement).trigger("touchstart").trigger("touchend");', queries.ios.activities.logoutButton),
-				web.wait(2000)
-			],
-			'android': [
+			'default': [
 				web.executeScript('$(targetElement).trigger("touchstart").trigger("touchend");', queries.ios.activities.logoutButton),
 				web.wait(2000)
 			]
 		},
 		'Then the Activities screen should be displayed' : {
-			'ios': [
-				web.getHtml({ className: 'km-view-title'}, function(result) {
-					assert(result.trim()).equals('Activities');
-				}),
-				web.wait(1000)
-			],
-			'android': [
+			'default': [
 				web.getHtml({ className: 'km-view-title'}, function(result) {
 					assert(result.trim()).equals('Activities');
 				}),
@@ -124,13 +104,7 @@ spec(function() {
 			]
 		},
 		'Then the activity should be posted': {
-			'ios': [
-				web.getHtml({ className: 'user-share-txt' }, function(result) {
-					assert(result.trim()).equals('Hello World.');
-				}),
-				web.wait(1000)
-			],
-			'android': [
+			'default': [
 				web.getHtml({ className: 'user-share-txt' }, function(result) {
 					assert(result.trim()).equals('Hello World.');
 				}),
@@ -138,12 +112,7 @@ spec(function() {
 			]
 		},
 		'Then the login screen should be displayed': {
-			'ios': [
-				web.getHtml(queries.ios.login.loginButtonText, function(result) {
-					assert(result.trim()).equals('Login');
-				})
-			],
-			'android': [
+			'default': [
 				web.getHtml(queries.ios.login.loginButtonText, function(result) {
 					assert(result.trim()).equals('Login');
 				})
