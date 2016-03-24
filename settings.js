8c7904919d7ddf9734b56f1ef16afb89357e56bc
gngeorgiev <gngeorgiev.it@gmail.com>
2016-03-24 16:42:44 +0200

    Fixed [SAMPLES] Missing default options for Login with 3rd party providers #311497

--- settings.js
+++ settings.js
@@ -6,22 +6,22 @@
         scheme: 'http',
         social: {
             facebook: {
-                appId: ''
+                appId: '1408629486049918' // Put your Facebook App ID here
             },
             twitter: {
-                acessToken: '',
-                accessTokenSecret: ''
+                acessToken: '3004026989-UzHGFbbxH9sDKEVPNUlBKtKoRqZ7q9Q3OCmRnLB', //Put your Twitter access token here
+                accessTokenSecret: 'rLyPa4XRvQEkvfLjGPCBitqRsqqCJCkGDZRo2eu6A6wh1' //Put your Twitter token secret here
             },
             google: {
-                clientId: ''
+                clientId: '406987471724-q1sorfhhcbulk6r5r317l482u9f62ti8.apps.googleusercontent.com' // Put your Google Client ID here
             },
             windows: {
-                clientId: '',
+                clientId: '000000004C10D1AF', // Put your LiveID Client ID here
                 redirect_uri: ''
             },
             adfs: {
-                endpoint: '',
-                realm: ''
+                endpoint: '$ADFS_ENDPOINT$', // Put your ADFS Realm here
+                realm: '$ADFS_REALM$' // Put your ADFS Endpoint here
             }
         }
     };
