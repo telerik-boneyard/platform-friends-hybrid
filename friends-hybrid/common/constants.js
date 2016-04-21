1822e2e32605c3c51eb2b0e754683f0d4424a772
gngeorgiev <gngeorgiev.it@gmail.com>
2016-04-21 14:15:51 +0300

    added 10mb limit when uploading files on device; now taking pictures from the camera instead of the library

--- friends-hybrid/common/constants.js
+++ friends-hybrid/common/constants.js
@@ -3,6 +3,7 @@
         defaultPicture: 'styles/img/default.jpg',
         whitePicture: 'styles/img/white.png',
         dateFormat: 'MMM d, yyyy',
-        simulatorFileSizeLimit: 3700000
+        simulatorFileSizeLimit: 3700000, //string length in base64, about 2.5mb
+        deviceFileSizeLimit: 1000000 * 10 //10mb
     };
 }());
